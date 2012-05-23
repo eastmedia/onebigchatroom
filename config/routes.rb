ea4253488417951fe53e0f29cc72f0e3443ce1c5
nodanaonlyzuul <stephen@eastmedia.com>
2012-05-23 12:11:26 -0400

    create a few basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Onebigchatroom::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :messages, :only => [:new, :create, :index, :destroy]
+
+  match '/chat' => 'messages#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
