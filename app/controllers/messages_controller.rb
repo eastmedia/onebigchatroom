06e4a9cc5bfe5cbdbaaa7dcbd4a6bb41c7c6911e
nodanaonlyzuul <stephen@eastmedia.com>
2012-05-23 12:37:28 -0400

    the base minimum to send and delete messages in /chat.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,11 @@
 class MessagesController < ApplicationController
+  def create
+    @message = Message.new(params[:message])
+    @message.save
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+  end
 end
