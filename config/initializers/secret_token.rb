4bd2a6b15be72042bfc2b46c59f29a7325b36556
nodanaonlyzuul <stephen@eastmedia.com>
2012-05-23 12:03:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onebigchatroom::Application.config.secret_token = '0abc9b18f12fb80687eb5c43b7af32e500032002006bce4b0b126376812a47f88d181eb804b91d3e202825f882c54fb967682a88d4b56cb516455c66ca9a05fb'
