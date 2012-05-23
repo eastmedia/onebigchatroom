268c2ec9655c0abba2ab66b39ad356f779f915cf
nodanaonlyzuul <stephen@eastmedia.com>
2012-05-23 13:05:58 -0400

    Use foreman to manage faye.

---
+++ Procfile
@@ -0,0 +1,1 @@
+faye_server: rackup faye.ru -s thin -E production
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def broadcast(channel, &block)
+    message = {:channel => channel, :data => capture(&:block)}
+    uri     = URI.parse("http://localhost:9292/faye")
+
+    Net::HTTP.post_form(uri, :message => message.to_json)
+  end
+
 end
