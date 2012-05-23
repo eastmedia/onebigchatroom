1814d2756986bde67908efddc410e93908273638
nodanaonlyzuul <stephen@eastmedia.com>
2012-05-23 13:30:26 -0400

    A big more work but that's it I promise.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
 module ApplicationHelper
 
   def broadcast(channel, &block)
-    message = {:channel => channel, :data => capture(&:block)}
+    message = {:channel => channel, :data => capture(&block)}
     uri     = URI.parse("http://localhost:9292/faye")
-
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
--- faye.ru
+++ faye.ru
@@ -1,3 +1,5 @@
 require 'faye'
+Faye::WebSocket.load_adapter('thin')
+
 faye_server = Faye::RackAdapter.new(:mount => "/faye", :timeout => 45)
 run faye_server
