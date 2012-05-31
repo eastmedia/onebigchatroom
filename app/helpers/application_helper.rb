15d806df0d44d9c58c9cafce5a6e39b034c62f61
nodanaonlyzuul <stephen@eastmedia.com>
2012-05-31 10:56:50 -0400

    helper method to know what domain the faye server resides on in production vs dev

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
 
+  def faye_host
+     Rails.env.production? ? "ec2-50-19-225-177.compute-1.amazonaws.com" : "localhost"
+  end
+
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block)}
-    uri     = URI.parse("http://localhost:9292/faye")
+    uri     = URI.parse("http://#{faye_host}:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
