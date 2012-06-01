e594c616ee89ebad36c19237811f77a846f686ac
pnts <pnts@protocol7.net>
2012-06-01 18:17:39 -0400

    non animated sidebar and POW for deletions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,23 +16,18 @@
 //= require_tree .
 
 
-$(function() {
+ $(function() {
 
-    var $sidebar   = $(".sidebar"),
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        topPadding = 35;
+  var $sidebar   = $(".sidebar"),
+      $window    = $(window),
+      offset     = $sidebar.offset(),
+      topPadding = 35;
 
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
-            });
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 20
-            });
-        }
-    });
-
+  $window.scroll(function() {
+    if ($window.scrollTop() > offset.top) {
+      $sidebar.addClass("fixed");
+    } else {
+      $sidebar.removeClass("fixed");
+    }
+  });
 });
