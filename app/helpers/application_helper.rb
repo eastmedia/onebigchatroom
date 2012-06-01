501c2f56ca28d0fbfc2b0d2a5300ecdd0e9322f4
nodanaonlyzuul <stephen@eastmedia.com>
2012-05-31 23:29:55 -0400

    temp work around to establish connection on production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
   def faye_host
-     Rails.env.production? ? "onebigchatroom.com" : "localhost"
+     Rails.env.production? ? "onebigchatroom.com:443" : "localhost:9292"
   end
 
   def broadcast(channel, &block)
     message = {:channel => channel, :data => capture(&block)}
-    uri     = URI.parse("http://#{faye_host}:9292/faye")
+    uri     = URI.parse("http://#{faye_host}/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
