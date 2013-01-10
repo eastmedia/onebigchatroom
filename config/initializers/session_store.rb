49e0b2a0a0a38e50410b9a0531b03f66c0a4fb8e
nodanaonlyzuul <stephen@eastmedia.com>
2013-01-10 11:33:40 -0500

    Updating to rails 3.2.10 for security reasons

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Onebigchatroom::Application.config.session_store :cookie_store, key: '_onebigchatroom_session'
+Onebigchatroom::Application.config.session_store :cookie_store, key: '_onebigchatroom_session', secret: "1be951ff10b6743d8c5d28e72d73daa7053d6214c148401f3ed31e50cbef818b63516c3362217462355cd5b58c46c6fff5b83886ab069f55b33858189fcb52e0"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
